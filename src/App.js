72665d88977823a616eb593bf31cdb264d85d914
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-12-31 23:25:02 +0900

    2.7 Themes
    다크모드
    하위 컴포넌트에서 상위 컴포넌트 속성인 Theme 가져온다.

--- src/App.js
+++ src/App.js
@@ -2,49 +2,22 @@ import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 
-  50% {
-    transform: rotate(360deg);
-    border-radius: 50%;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 30px;
-`;
-
-const Box = styled.div`
-  display: flex;
   justify-content: center;
   align-items: center;
-  background-color: skyblue;
-  width: 100px;
-  height: 100px;
-  animation: ${rotationAnimation} 3s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 70px;
-    }
-  }
+  height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Father>
-      <Box>
-        <Emoji>💯</Emoji>
-      </Box>
-      <Emoji>♻</Emoji>
+      <Title>React</Title>
     </Father>
   );
 }
