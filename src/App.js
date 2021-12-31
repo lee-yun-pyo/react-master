b564ba6485e12bee5dfa4594f5ff4fc9f05ab2e5
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2021-12-31 19:16:58 +0900

    2.2 Extending
    background-color: ${(props) => props.bgColor};
    const Circle = styled(Box)`

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne></BoxOne>
-      <BoxTwo></BoxTwo>
+      <Box bgColor="teal"></Box>
+      <Circle bgColor="tomato"></Circle>
     </Father>
   );
 }
