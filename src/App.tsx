2efaf2e553fea4eee0238c30e2e993ccd38e6188
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-02 00:50:54 +0900

    3.1 DefinetelyTyped(npx create-react-app~)
    npx create-react-app my app --template typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import styled, { keyframes } from "styled-components";
-
-const Father = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  font-size: 30px;
-  color: ${(props) => props.theme.textColor};
-`;
-
 function App() {
   return (
-    <Father>
-      <Title>React</Title>
-    </Father>
+    <div>
+
+    </div>
   );
 }
 
