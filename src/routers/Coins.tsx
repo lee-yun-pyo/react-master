189acf439a70612450bbeec0a0990c5c51d487d0
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-02 19:05:49 +0900

    4.3 Crypto Tracker - Home part 1
    - Link

--- src/routers/Coins.tsx
+++ src/routers/Coins.tsx
@@ -1,11 +1,83 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+    padding: 0 20px;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+    background-color: white;
+    color: ${props=>props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    a {
+        padding: 15px;
+        transition: color 0.2s ease-in;
+        display: block;
+    }
+    &:hover {
+        color: ${props=>props.theme.accentColor};
+    }
+`;
 
 const Title = styled.h1`
+    font-size: 35px;
     color: ${props=>props.theme.accentColor};
 `;
 
+const coins = [
+    {
+    "id": "btc-bitcoin",
+    "name": "Bitcoin",
+    "symbol": "BTC",
+    "rank": 1,
+    "is_new": false,
+    "is_active": true,
+    "type": "coin"
+  },
+  {
+    "id": "eth-ethereum",
+    "name": "Ethereum",
+    "symbol": "ETH",
+    "rank": 2,
+    "is_new": false,
+    "is_active": true,
+    "type": "coin"
+  },
+  {
+    "id": "bnb-binance-coin",
+    "name": "Binance Coin",
+    "symbol": "BNB",
+    "rank": 3,
+    "is_new": false,
+    "is_active": true,
+    "type": "coin"
+  },
+]
+
 function Coins() {
-    return <Title>Coins</Title>
+    return (
+        <Container>
+            <Header>
+                <Title>Coins</Title>
+            </Header>
+            <CoinsList>
+                {coins.map(coin => 
+                <Coin key={coin.id}>
+                    <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                </Coin>)}
+            </CoinsList>
+        </Container>
+    )
 }
 
 export default Coins;
