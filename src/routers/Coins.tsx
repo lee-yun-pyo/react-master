f8e2eb29dc8cfa7acf0f22d694ef177d8cdaa4f2
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-02 19:36:28 +0900

    4.3 Crypto Tracker - Home part 2
    - useEffect
    - margin: 0 auto;
    - fetch API

--- src/routers/Coins.tsx
+++ src/routers/Coins.tsx
@@ -1,83 +1,86 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
-    padding: 0 20px;
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto; /* 센터로 옴. */
 `;
 
 const Header = styled.header`
-    height: 10vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-    background-color: white;
-    color: ${props=>props.theme.bgColor};
-    margin-bottom: 10px;
-    border-radius: 15px;
-    a {
-        padding: 15px;
-        transition: color 0.2s ease-in;
-        display: block;
-    }
-    &:hover {
-        color: ${props=>props.theme.accentColor};
-    }
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  a {
+    padding: 15px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
 `;
 
 const Title = styled.h1`
-    font-size: 35px;
-    color: ${props=>props.theme.accentColor};
+  font-size: 35px;
+  color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-    "id": "btc-bitcoin",
-    "name": "Bitcoin",
-    "symbol": "BTC",
-    "rank": 1,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin"
-  },
-  {
-    "id": "eth-ethereum",
-    "name": "Ethereum",
-    "symbol": "ETH",
-    "rank": 2,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin"
-  },
-  {
-    "id": "bnb-binance-coin",
-    "name": "Binance Coin",
-    "symbol": "BNB",
-    "rank": 3,
-    "is_new": false,
-    "is_active": true,
-    "type": "coin"
-  },
-]
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface coinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
-    return (
-        <Container>
-            <Header>
-                <Title>Coins</Title>
-            </Header>
-            <CoinsList>
-                {coins.map(coin => 
-                <Coin key={coin.id}>
-                    <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-                </Coin>)}
-            </CoinsList>
-        </Container>
-    )
+  const [coins, setCoins] = useState<coinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
 
 export default Coins;
