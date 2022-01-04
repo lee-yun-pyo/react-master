f66528292a5f1516eb89f21c7233bd72e20eb52f
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-04 21:03:10 +0900

    4.12 Crypto Tracker - Price Chart
    - fetchChartData 생성
    - useQuery("...", fetchChartData(coinId)); 생성

--- src/routers/Chart.tsx
+++ src/routers/Chart.tsx
@@ -1,4 +1,15 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { useParams } from "react-router";
+import { fetchChartData } from "../api";
+
+interface ICoinId {
+  coinId: string;
+}
+
+function Chart({ coinId }: ICoinId) {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchChartData(coinId)
+  );
   return <div>Chart</div>;
 }
 
--- src/routers/Coin.tsx
+++ src/routers/Coin.tsx
@@ -167,7 +167,7 @@ function Coin() {
           <Price />
         </Route>
         <Route path={`/${coinId}/chart`}>
-          <Chart />
+          <Chart coinId={coinId} />
         </Route>
       </Switch>
     </Container>
