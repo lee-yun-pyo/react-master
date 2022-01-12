84e7677fe084811440e0c5ef1baad3a5e29fab40
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-12 16:56:45 +0900

    #5.6 Forms - react-hook-form
    react-hook-form 설치
    register, watch 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setTodo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           type="text"
+//           placeholder="write your to do"
+//         />
+//         <button>클릭</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write your to do"
-        />
-        <button>클릭</button>
+      <form>
+        <input {...register("Email")} type="text" placeholder="write" />
+        <input {...register("Email2")} type="text" placeholder="write" />
+        <input {...register("Email3")} type="text" placeholder="write" />
+        <input {...register("Email4")} type="text" placeholder="write" />
+        <input {...register("Email5")} type="text" placeholder="write" />
       </form>
     </div>
   );
