a20fc18cfc4febcca697496a43b79e049d0d5db4
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-15 23:19:41 +0900

    #5.11 Add To Do
    - ITodo interface 추가 (category 포함)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,29 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 
 interface ITodo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<ITodo[]>({
+  key: "toDos",
+  default: [],
+});
+
+interface IForm {
   toDo: string;
 }
 
 function ToDoList1() {
-  const { register, handleSubmit, setValue } = useForm<ITodo>();
-  const onSubmit = (data: ITodo) => {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const onSubmit = ({ toDo }: IForm) => {
+    setToDos((oldToDo) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDo,
+    ]);
     setValue("toDo", "");
   };
   return (
@@ -23,6 +40,11 @@ function ToDoList1() {
         />
         <button>클릭</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
