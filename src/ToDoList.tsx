d921f4012ccd091807ae408825efb8453f7c00a6
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-01-17 01:44:54 +0900

    #5.17 Selector

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,20 +1,28 @@
+import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoState } from "./components/atoms";
+import { categoryState, toDoSelector, toDoState } from "./components/atoms";
 import CreateToDo from "./components/CreateToDo";
 import ToDo from "./components/ToDo";
 
 function ToDoList1() {
-  const toDos = useRecoilValue(toDoState);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>투두 리스트</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">TO DO</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">DONE</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo}></ToDo>
+      ))}
     </div>
   );
 }
--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface ITodo {
   text: string;
@@ -9,4 +9,23 @@ export interface ITodo {
 export const toDoState = atom<ITodo[]>({
   key: "toDos",
   default: [],
+});
+
+export const categoryState = atom({
+  key: "categoryState",
+  default: "TO_DO",
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    if (category === "TO_DO")
+      return toDos.filter((toDo) => toDo.category === "TO_DO");
+    if (category === "DOING")
+      return toDos.filter((toDo) => toDo.category === "DOING");
+    if (category === "DONE")
+      return toDos.filter((toDo) => toDo.category === "DONE");
+  },
 });
